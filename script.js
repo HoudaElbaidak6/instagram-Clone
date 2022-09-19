7657c9a383582a0931add0b063930a532d03affb
Houda Elbaidak <houdaelbaidak6@gmail.com>
2022-09-19 11:20:52 +0100

    okay that's done

--- script.js
+++ script.js
@@ -3,28 +3,37 @@ let buttons = document.querySelectorAll("button");
 let body = document.querySelector("body");
 let toggle = document.querySelector("header>i");
 let outputs = document.querySelectorAll("p");
-let likeCount = 0;
-let dislikeCount = 0;
 
+let likeCount = localStorage.getItem("likes") == null ? 0 : localStorage.getItem("likes");
+outputs[0].textContent = likeCount;
+let dislikeCount = localStorage.getItem("dislikes") == null ? 0 : localStorage.getItem("dislikes");
+outputs[1].textContent = likeCount;
+// let dislikeCount = 0;
+
+/* let like = localStorage.getItem("likes");
+likeCount.textContent = like;
+ */
 img.addEventListener("dblclick", function () {
-  
   console.log("double-click works");
-  likeCount++;
+  ++likeCount;
   console.log(likeCount);
-  // outputs[0].textContent = likeCount;
-  likeCounter();
+  outputs[0].innerHTML = likeCount;
+  // likeCounter();
+  localStorage.setItem("likes", likeCount);
 });
 
 buttons[1].addEventListener("click", function () {
   dislikeCount++;
   outputs[1].textContent = dislikeCount;
-  dislikeCounter();
+  // dislikeCounter();
+  localStorage.setItem("dislikes", dislikeCount);
 });
 
 buttons[0].addEventListener("click", function () {
   likeCount++;
-  // outputs[0].textContent = likeCount;
-  likeCounter();
+  outputs[0].textContent = likeCount;
+  // likeCounter();
+  localStorage.setItem("likes", likeCount);
 });
 
 toggle.addEventListener("click", function () {
@@ -37,7 +46,7 @@ toggle.addEventListener("click", function () {
   }
 });
 
-function likeCounter() {
+/* function likeCounter() {
   if (localStorage.likeCount) {
     localStorage.likeCount = Number(localStorage.likeCount) + 1;
   } else {
@@ -53,5 +62,5 @@ function dislikeCounter() {
     localStorage.dislikeCount = 1;
   }
   outputs[1].innerHTML = localStorage.dislikeCount;
-}
+} */
 // localStorage.setItem("likes",JSON.stringify(likeCount))
