fa67701ef60238d5d2364f11b56d5fa769fa2d46
Houda Elbaidak <houdaelbaidak6@gmail.com>
2022-09-19 10:35:29 +0100

    k

--- script.js
+++ script.js
@@ -2,18 +2,12 @@ let img = document.querySelector("img");
 let buttons = document.querySelectorAll("button");
 let body = document.querySelector("body");
 let toggle = document.querySelector("header>i");
-
-/* let likeBtn = document.getElementById("likeBtn");
-let dislikeBtn = document.getElementById("dislikeBtn"); */
 let outputs = document.querySelectorAll("p");
-
-/* let likeOutput = document.getElementById("likesP");
-let dislikeOutput = document.getElementById("dislikeP"); */
 let likeCount = 0;
 let dislikeCount = 0;
 
 img.addEventListener("dblclick", function () {
-  //   alert("it was double clicked");
+  
   console.log("double-click works");
   likeCount++;
   console.log(likeCount);
